e90665a4e83b2985af9ce445cf83359e599ca10f
laetitiaouelle <ouelle123ab@gmail.com>
2022-10-31 22:22:46 +0000

    latest update

--- src/section/Pricing/index.js
+++ src/section/Pricing/index.js
@@ -1,8 +1,32 @@
-import React from 'react'
+import React, { useState }  from 'react'
+
+
 
 function Pricing() {
+
+  const [isMonth, setisMonth] = useState(false)
+  const setIsMonthly = ()=>setisMonth(!isMonth)  
+
   return (
-    <div>Pricing</div>
+    <div className='mx-[135px]  mt-[140px]'>
+      <div className='w-full mb-[34px] ' >
+        <span className='flex item-center justify-center font-SatoshiMedium text-black text-[56px] ' >
+          Choose your plan
+        </span>
+      </div>
+      <div className='w-full'>
+        <span className='flex item-center justify-center' >
+            <a className=' w-[263px] h-12 bg-[#EEEDF0] flex items-center justify-between rounded-[42px] px-1 relative' >
+                <span className='h-10 flex items-center justify-center rounded-[42px] w-[162px] absolute left-0 '>
+                    Monthly
+                </span>
+                <span className='h-10 flex items-center justify-center bg-black text-white rounded-[42px] w-[162px] absolute right-0 '>
+                    Yearly Save 50%
+                </span>
+            </a>
+        </span>
+      </div>
+    </div>
   )
 }
 
