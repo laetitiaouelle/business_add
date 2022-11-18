0fb4ae637d95ffb293d129316ec0e0503f8f3c0e
Laetitia Christelle Ouelle <laetitia@kaanari.com>
2022-11-18 13:49:13 +0000

    latest update

--- src/section/FAQ/index.js
+++ src/section/FAQ/index.js
@@ -3,7 +3,7 @@ import React from 'react'
 function FAQ() {
   return (
     <div className='mx-[135px]  mt-[140px]'>
-      <div className='w-full '>
+      <div className='w-full'>
         <span className='flex item-center justify-center font-SatoshiMedium text-black text-[56px] mb-[54px]' >
           Frequently Asked Questions
         </span>
@@ -35,6 +35,11 @@ function FAQ() {
             How can I manage my inefficient ads that I have identified with A/B testing?
           </span>
           <span className=' h-[1px] bg-[#A7A7A7]  block w-full mb-[52px] ' ></span>
+        </div>
+        <div className='w-full flex items-center justify-center' >
+          <a className='flex item-center justify-center w-[196px] h-[60px] bg-black group hover:border hover:border-black hover:bg-transparent cursor-pointer' >
+            <span className=' font-SatoshiMedium text-white text-[17px] flex justify-center items-center group-hover:text-black ' >Show More</span>
+          </a>
         </div>
       </div>
     </div>
